511d9859237af66948c1cc03e487f43b17f94772
Raphael Burgstaller <scodusoft@gmail.com>
2020-08-11 11:07:27 +0200

    added react

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,8 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
+mix.react('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css');
+mix.browserSync('localhost:8000');
+//Add your own php artisan serve's url and port
+//For me url is localhost or 127.0.0.1 and port is 8000
