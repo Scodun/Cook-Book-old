82f3862da0b62739e73a41ef16199fd2332d4794
Raphael Burgstaller <scodusoft@gmail.com>
2020-08-11 19:41:36 +0200

    added index

---
+++ resources/js/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Layout } from 'antd';
+import ReactDOM from "react-dom";
+import {
+    BrowserRouter as Router,
+} from "react-router-dom";
+import { ScrollToTop } from "./components/atoms";
+import CustomFooter from "./components/molecules/Footer";
+import Routes from "./Routes";
+
+
+class Index extends React.Component {
+    render() {
+        return (
+            <Router>
+                <ScrollToTop>
+                    <div className="flyout">
+
+                        <main>
+                            <Routes />
+                        </main>
+                        <CustomFooter />
+                    </div>
+                </ScrollToTop>
+            </Router>
+        );
+    }
+}
+
+export default Index;
+
+if (document.getElementById('index')) {
+    ReactDOM.render(<Index />, document.getElementById('index'));
+}
