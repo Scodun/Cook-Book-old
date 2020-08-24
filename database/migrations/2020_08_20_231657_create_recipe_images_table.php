f55e412a632e32df6b4ab9f98825124e01ff4530
Raphael Burgstaller <scodusoft@gmail.com>
2020-08-24 21:47:51 +0200

    migration fixes

--- database/migrations/2020_08_20_231657_create_recipe_images_table.php
+++ database/migrations/2020_08_20_231657_create_recipe_images_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateIngredientsRecipeImagesTable extends Migration
+class CreateRecipeImagesTable extends Migration
 {
     /**
      * Run the migrations.
