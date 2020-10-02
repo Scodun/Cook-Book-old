e1c0553ac11e935def2da8c29a6aca8c7b12d3d3
Raphael Burgstaller <scodusoft@gmail.com>
2020-10-03 00:38:01 +0200

    add - cookie for accept

--- resources/js/components/molecules/CookieMessage/index.jsx
+++ resources/js/components/molecules/CookieMessage/index.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { Button, notification } from "antd";
 import { withRouter } from "react-router-dom";
+import Cookies from "js-cookie";
 
 class CookieMessage extends React.Component {
   componentDidMount () {
     const key = "openCookie";
     const btn = (
       <div>
-        <Button type="primary" size="small" className="mr-1" onClick={() => notification.close(key)}>
+        <Button type="primary" size="small" className="mr-1" onClick = {this.onClickAccept}>
           {trans("general.confirm")}
         </Button>
 
@@ -16,17 +17,24 @@ class CookieMessage extends React.Component {
         </Button>
       </div>
     );
-    notification.open({
-      message: "Cookies 🍪",
-      description: trans("general.cookie_message"),
-      btn,
-      key,
-      duration: 0
-    });
+    if (!Cookies.get("cookies")) {
+      notification.open({
+        message: "Cookies 🍪",
+        description: trans("general.cookie_message"),
+        btn,
+        key,
+        duration: 0
+      });
+    }
   }
 
     onClickRedirect = () => {
       this.props.history.push("/legal");
+    }
+
+    onClickAccept=() => {
+      notification.close("openCookie");
+      Cookies.set("cookies", true);
     }
 
     render () {
